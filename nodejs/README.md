2a88759a8144fd1b770486ce17b48dd893d43e0a
David Liu <david-khala@hotmail.com>
2023-11-09 12:22:11 +0800

    0.0.0: init commit

---
+++ nodejs/README.md
@@ -0,0 +1,5 @@
+# @davidkhala/container
+
+
+
+
---
+++ nodejs/test/syntaxTest.js
@@ -0,0 +1,10 @@
+import assert from 'assert';
+import {OCIContainerOptsBuilder} from '../oci.js'
+describe('container Opts builder', () => {
+	it('constructor', () => {
+		assert.throws(() => {
+			new OCIContainerOptsBuilder('image', 'sh');
+		});
+
+	});
+});
