03fd496a8b99843701e371c762d371200e818e97
David Liu <david-khala@hotmail.com>
2025-01-03 14:37:41 +0800

    Create health.sh

---
+++ cli/health.sh
@@ -0,0 +1,19 @@
+wait-until-healthy(){
+  
+  local CONTAINER_NAME=$1
+
+  # Loop until the container is healthy
+  while true; do
+      HEALTH_STATUS=$(docker inspect --format='{{.State.Health.Status}}' "$CONTAINER_NAME")
+  
+      if [[ "$HEALTH_STATUS" == "healthy" ]]; then
+          echo "Container is healthy!"
+          break
+      elif [[ "$HEALTH_STATUS" == "unhealthy" ]]; then
+          echo "Container is unhealthy!"
+          exit 1
+      else
+          echo "Current health status: $HEALTH_STATUS. Waiting..."
+      fi
+  done
+}
