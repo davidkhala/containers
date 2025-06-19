b53fd749b2dab033baac16b96083c10890230790
david <david-khala@hotmail.com>
2025-06-19 10:57:18 +0800

    add run case

--- nodejs/oci.js
+++ nodejs/oci.js
@@ -1,6 +1,7 @@
 import Dockerode from 'dockerode';
 import assert from 'assert';
 import {Reason, ContainerStatus} from './constants.js';
+import {Writable} from 'stream'
 
 const {ContainerNotFound, VolumeNotFound, NetworkNotFound, ImageNotFound} = Reason;
 const {exited, running, created} = ContainerStatus;
@@ -22,260 +23,297 @@ const {exited, running, created} = ContainerStatus;
  * Open Container Initiative: OCI
  */
 export default class OCI {
-	/**
-	 *
-	 * @param {DockerodeOpts} [opts]
-	 * @param [logger]
-	 */
-	constructor(opts, logger = console) {
-		if (opts && !opts.protocol && opts.host) {
-			opts.protocol = 'ssh';
-		}
+    /**
+     *
+     * @param {DockerodeOpts} [opts]
+     * @param [logger]
+     */
+    constructor(opts, logger = console) {
+        if (opts && !opts.protocol && opts.host) {
+            opts.protocol = 'ssh';
+        }
 
-		this.client = new Dockerode(opts);
-		this.logger = logger;
-	}
+        this.client = new Dockerode(opts);
+        this.logger = logger;
+    }
 
-	async info() {
-		return this.client.info();
-	}
+    async info() {
+        return this.client.info();
+    }
 
-	async ping() {
-		const result = await this.client.ping();
-		return result.toString();
-	}
+    async ping() {
+        const result = await this.client.ping();
+        return result.toString();
+    }
 
-	async systemPrune() {
-		await this.client.pruneContainers();
-		await this.client.pruneVolumes();
-		await this.client.pruneNetworks();
-	}
+    async systemPrune() {
+        await this.client.pruneContainers();
+        await this.client.pruneVolumes();
+        await this.client.pruneNetworks();
+    }
 
-	/**
-	 *
-	 * @param Name
-	 * @param path
-	 */
-	async volumeCreateIfNotExist({Name, path}) {
-		return this.client.createVolume({
-			Name, Driver: 'local', DriverOpts: {
-				o: 'bind', device: path, type: 'none'
-			}
-		});
-	}
+    /**
+     *
+     * @param Name
+     * @param path
+     */
+    async volumeCreateIfNotExist({Name, path}) {
+        return this.client.createVolume({
+            Name, Driver: 'local', DriverOpts: {
+                o: 'bind', device: path, type: 'none'
+            }
+        });
+    }
 
-	async volumeRemove(Name) {
-		try {
-			const volume = this.client.getVolume(Name);
-			const info = await volume.inspect();
-			this.logger.info('delete volume', Name);
-			this.logger.debug('delete volume', info);
-			return await volume.remove();
-		} catch (err) {
-			if (err.statusCode === 404 && err.reason === VolumeNotFound) {
-				this.logger.info(err.json.message, 'delete skipped');
-			} else {
-				throw err;
-			}
-		}
-	}
+    async volumeRemove(Name) {
+        try {
+            const volume = this.client.getVolume(Name);
+            const info = await volume.inspect();
+            this.logger.info('delete volume', Name);
+            this.logger.debug('delete volume', info);
+            return await volume.remove();
+        } catch (err) {
+            if (err.statusCode === 404 && err.reason === VolumeNotFound) {
+                this.logger.info(err.json.message, 'delete skipped');
+            } else {
+                throw err;
+            }
+        }
+    }
 
-	/**
-	 *
-	 * @param {string} containerName
-	 */
-	async containerDelete(containerName) {
-		const container = this.client.getContainer(containerName);
-		try {
-			const containInfo = await container.inspect();
-			const currentStatus = containInfo.State.Status;
-			this.logger.debug('delete container', containerName, currentStatus);
-			if (this._beforeKill().includes(currentStatus)) {
-				await container.kill();
-			}
-			return await container.remove();
+    /**
+     *
+     * @param {string} containerName
+     */
+    async containerDelete(containerName) {
+        const container = this.client.getContainer(containerName);
+        try {
+            const containInfo = await container.inspect();
+            const currentStatus = containInfo.State.Status;
+            this.logger.debug('delete container', containerName, currentStatus);
+            if (this._beforeKill().includes(currentStatus)) {
+                await container.kill();
+            }
+            return await container.remove();
 
-		} catch (err) {
-			if (err.statusCode === 404 && err.reason === ContainerNotFound) {
-				this.logger.info(err.json.message, 'deleting skipped');
-			} else {
-				throw err;
-			}
-		}
-	}
+        } catch (err) {
+            if (err.statusCode === 404 && err.reason === ContainerNotFound) {
+                this.logger.info(err.json.message, 'deleting skipped');
+            } else {
+                throw err;
+            }
+        }
+    }
 
-	/**
-	 * @param {ContainerOpts} createOptions
-	 * @param {boolean} [imagePullIfNotExist]
-	 */
-	async containerStart(createOptions, imagePullIfNotExist) {
-		const {name: containerName, Image} = createOptions;
-		if (imagePullIfNotExist) {
-			await this.imagePullIfNotExist(Image);
-		}
-		let container = this.client.getContainer(containerName), info;
+    async run(Image, Cmd, redirect) {
 
-		try {
-			info = await container.inspect();
-			this.logger.info('container found', {containerName, status: info.State.Status});
+        if (redirect) {
+            // TODO validate and how about process stderr
+            const container = await this.client.createContainer({
+                Image,
+                Cmd,
+                AttachStdout: true,
+                AttachStderr: true,
+            });
+            await container.start();
+            const stream = await container.attach({stream: true, stdout: true, stderr: true});
+            this.client.modem.demuxStream(stream, process.stdout, process.stderr);
+            await container.wait();
+            await container.remove()
+        } else {
+            let stdoutData = '', stderrData = ''
+            const stdoutStream = new Writable({
+                write(chunk, encoding, callback) {
+                    stdoutData += chunk.toString();
+                    callback();
+                }
+            });
 
-		} catch (err) {
-			if (err.reason === ContainerNotFound && err.statusCode === 404) {
-				this.logger.info(err.json.message);
-				this.logger.info(`creating container [${containerName}]`);
-				container = await this.client.createContainer(createOptions);
-				info = await container.inspect();
-			} else {
-				throw err;
-			}
-		}
-		if (this._afterCreate().includes(info.State.Status)) {
-			await container.start();
-			info = await container.inspect();
-			assert.ok(this._afterStart().includes(info.State.Status), `should be one of [${this._afterStart()}], but got status ${info.State.Status}`);
-		}
-		return info;
-	}
+            const stderrStream = new Writable({
+                write(chunk, encoding, callback) {
+                    stderrData += chunk.toString();
+                    callback();
+                }
+            });
+            await this.client.run(image, commands, [
+                stdoutStream, stderrStream,
+            ], {Tty: false});
+            return [stdoutData.trim(), stderrData.trim()]
+        }
+    }
 
-	async containerInspect(containerName) {
-		const container = this.client.getContainer(containerName);
-		return await container.inspect();
-	}
+    /**
+     * @param {ContainerOpts} createOptions
+     * @param {boolean} [imagePullIfNotExist]
+     */
+    async containerStart(createOptions, imagePullIfNotExist) {
+        const {name: containerName, Image} = createOptions;
+        if (imagePullIfNotExist) {
+            await this.imagePullIfNotExist(Image);
+        }
+        let container = this.client.getContainer(containerName), info;
 
-	/**
-	 * expected status before healthy
-	 * @abstract
-	 * @private
-	 * @return string[]
-	 */
-	_beforeHealthy() {
-		return ['starting', 'unhealthy'];
-	}
+        try {
+            info = await container.inspect();
+            this.logger.info('container found', {containerName, status: info.State.Status});
 
-	async containerWaitForHealthy(containerName) {
-		const info = await this.containerInspect(containerName);
-		assert.ok(info.State.Health, 'health check section configuration not specified yet');
-		if (!OCI.isContainerHealthy(info)) {
-			const current = info.State.Health.Status;
-			assert.ok(this._beforeHealthy().includes(current), `expected status is either ${this._beforeHealthy()}, but got [${current}]`);
-			return this.containerWaitForHealthy(containerName);
-		}
-		return info;
-	}
+        } catch (err) {
+            if (err.reason === ContainerNotFound && err.statusCode === 404) {
+                this.logger.info(err.json.message);
+                this.logger.info(`creating container [${containerName}]`);
+                container = await this.client.createContainer(createOptions);
+                info = await container.inspect();
+            } else {
+                throw err;
+            }
+        }
+        if (this._afterCreate().includes(info.State.Status)) {
+            await container.start();
+            info = await container.inspect();
+            assert.ok(this._afterStart().includes(info.State.Status), `should be one of [${this._afterStart()}], but got status ${info.State.Status}`);
+        }
+        return info;
+    }
 
-	static isContainerHealthy(containerInfo) {
-		return containerInfo.State.Health?.Status === 'healthy';
-	}
+    async containerInspect(containerName) {
+        const container = this.client.getContainer(containerName);
+        return await container.inspect();
+    }
 
-	async containerList({all, network, status} = {all: true}) {
-		const filters = {
-			network: network ? [network] : undefined, status: status ? [status] : undefined
-		};
-		return this.client.listContainers({all, filters});
-	}
+    /**
+     * expected status before healthy
+     * @abstract
+     * @private
+     * @return string[]
+     */
+    _beforeHealthy() {
+        return ['starting', 'unhealthy'];
+    }
 
-	async inflateContainerName(container_name) {
-		const containers = await this.containerList();
-		return containers.filter(container => container.Names.find(name => name.includes(container_name)));
-	}
+    async containerWaitForHealthy(containerName) {
+        const info = await this.containerInspect(containerName);
+        assert.ok(info.State.Health, 'health check section configuration not specified yet');
+        if (!OCI.isContainerHealthy(info)) {
+            const current = info.State.Health.Status;
+            assert.ok(this._beforeHealthy().includes(current), `expected status is either ${this._beforeHealthy()}, but got [${current}]`);
+            return this.containerWaitForHealthy(containerName);
+        }
+        return info;
+    }
 
-	async networkRemove(Name) {
-		try {
-			const network = this.client.getNetwork(Name);
-			await network.inspect();
-			return await network.remove();
-		} catch (err) {
-			if (err.statusCode === 404 && err.reason === NetworkNotFound) {
-				this.logger.info(err.json.message, 'deleting skipped');
-			} else {
-				throw err;
-			}
-		}
-	}
+    static isContainerHealthy(containerInfo) {
+        return containerInfo.State.Health?.Status === 'healthy';
+    }
 
-	async imagePrune() {
-		await this.client.pruneImages();
-	}
+    async containerList({all, network, status} = {all: true}) {
+        const filters = {
+            network: network ? [network] : undefined, status: status ? [status] : undefined
+        };
+        return this.client.listContainers({all, filters});
+    }
 
-	async imageList(opts = {all: undefined}) {
-		return this.client.listImages(opts);
-	}
+    async inflateContainerName(container_name) {
+        const containers = await this.containerList();
+        return containers.filter(container => container.Names.find(name => name.includes(container_name)));
+    }
 
-	async imagePullIfNotExist(imageName) {
-		const image = this.client.getImage(imageName);
-		try {
-			return await image.inspect();
-		} catch (err) {
-			if (err.statusCode === 404 && err.reason === ImageNotFound) {
-				this.logger.debug(err.json.message, 'pulling');
-				await this.imagePull(imageName);
-				return await image.inspect();
-			} else {
-				throw err;
-			}
-		}
-	}
+    async networkRemove(Name) {
+        try {
+            const network = this.client.getNetwork(Name);
+            await network.inspect();
+            return await network.remove();
+        } catch (err) {
+            if (err.statusCode === 404 && err.reason === NetworkNotFound) {
+                this.logger.info(err.json.message, 'deleting skipped');
+            } else {
+                throw err;
+            }
+        }
+    }
 
-	async imageDelete(imageName) {
-		try {
-			const image = this.client.getImage(imageName);
-			const imageInfo = await image.inspect();
-			this.logger.info('delete image', imageInfo.RepoTags);
-			return await image.remove({force: true});
-		} catch (err) {
-			if (err.statusCode === 404 && err.reason === ImageNotFound) {
-				this.logger.debug(err.json.message, 'skip deleting');
-			} else {
-				throw err;
-			}
-		}
-	}
+    async imagePrune() {
+        await this.client.pruneImages();
+    }
 
-	async imagePull(imageName, onProgressCallback) {
+    async imageList(opts = {all: undefined}) {
+        return this.client.listImages(opts);
+    }
 
-		const stream = await this.client.pull(imageName);
-		return new Promise((resolve, reject) => {
-			const onFinished = (err, output) => {
-				if (err) {
-					this.logger.error('pull image error', {err, output});
-					return reject(err);
-				} else {
-					return resolve(output);
-				}
-			};
-			this.client.modem.followProgress(stream, onFinished, onProgressCallback);
-		});
+    async imagePullIfNotExist(imageName) {
+        const image = this.client.getImage(imageName);
+        try {
+            return await image.inspect();
+        } catch (err) {
+            if (err.statusCode === 404 && err.reason === ImageNotFound) {
+                this.logger.debug(err.json.message, 'pulling');
+                await this.imagePull(imageName);
+                return await image.inspect();
+            } else {
+                throw err;
+            }
+        }
+    }
 
-	}
+    async imageDelete(imageName) {
+        try {
+            const image = this.client.getImage(imageName);
+            const imageInfo = await image.inspect();
+            this.logger.info('delete image', imageInfo.RepoTags);
+            return await image.remove({force: true});
+        } catch (err) {
+            if (err.statusCode === 404 && err.reason === ImageNotFound) {
+                this.logger.debug(err.json.message, 'skip deleting');
+            } else {
+                throw err;
+            }
+        }
+    }
 
-	/**
-	 * expected status after container created
-	 * @abstract
-	 * @private
-	 * @return string[]
-	 */
-	_afterCreate() {
-		return [created];
-	}
+    async imagePull(imageName, onProgressCallback) {
 
-	/**
-	 * expected status after container started
-	 * @abstract
-	 * @private
-	 * @return string[]
-	 */
-	_afterStart() {
-		return [running, exited];
-	}
+        const stream = await this.client.pull(imageName);
+        return new Promise((resolve, reject) => {
+            const onFinished = (err, output) => {
+                if (err) {
+                    this.logger.error('pull image error', {err, output});
+                    return reject(err);
+                } else {
+                    return resolve(output);
+                }
+            };
+            this.client.modem.followProgress(stream, onFinished, onProgressCallback);
+        });
 
-	/**
-	 * expected status before killing container
-	 * @abstract
-	 * @private
-	 * @return string[]
-	 */
-	_beforeKill() {
-		return [running];
-	}
+    }
+
+    /**
+     * expected status after container created
+     * @abstract
+     * @private
+     * @return string[]
+     */
+    _afterCreate() {
+        return [created];
+    }
+
+    /**
+     * expected status after container started
+     * @abstract
+     * @private
+     * @return string[]
+     */
+    _afterStart() {
+        return [running, exited];
+    }
+
+    /**
+     * expected status before killing container
+     * @abstract
+     * @private
+     * @return string[]
+     */
+    _beforeKill() {
+        return [running];
+    }
 }
 
